6f29a40ef1b6f2a47e9f6ece91b23f305f7fcb18
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-05-14 17:58:47 +0500

    fix: remove unused default transloco config

--- transloco.config.ts
+++
@@ -1,9 +0,0 @@
-import type { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
-
-const config: TranslocoGlobalConfig = {
-  rootTranslationsPath: 'public/i18n/',
-  langs: ['en', 'ru'],
-  keysManager: {},
-};
-
-export default config;
