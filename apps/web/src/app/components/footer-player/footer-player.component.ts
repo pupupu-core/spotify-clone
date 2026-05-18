3f8066fd2293d20698ced5a24d6f673a2cf1a728
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-05-19 01:23:51 +0500

    feat: add empty footer-player component

---
+++ apps/web/src/app/components/footer-player/footer-player.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ppf-footer-player',
+  imports: [],
+  templateUrl: './footer-player.component.html',
+  styleUrl: './footer-player.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterPlayerComponent {}
