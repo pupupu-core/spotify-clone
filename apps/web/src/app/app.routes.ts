644d5580eeafc46c8d3e190a5003024a92d4d0f3
tryproxy <tryproxy@protonmail.com>
2026-05-18 18:16:22 +0400

    feat: set up routing, add empty home, auth and 404 pages

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import type { Route } from '@angular/router';
+import { ROUTES } from './shared/config/routes';
+import { AppLayoutComponent as AppLayout } from './components/layout/app-layout/app-layout.component';
+import { HomePageComponent as HomePage } from './pages/home/home-page.component';
+import { AuthPageComponent as AuthPage } from './pages/auth/auth-page.component';
+import { loadComponent } from './shared/lib/load-component';
 
-export const appRoutes: Route[] = [];
+const notFoundPage = loadComponent(() =>
+  import('./pages/not-found/not-found-page.component').then(
+    ({ NotFoundPageComponent }) => NotFoundPageComponent,
+  ),
+);
+
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: AppLayout,
+    children: [
+      {
+        path: ROUTES.HOME.path,
+        title: ROUTES.HOME.meta.title,
+        component: HomePage,
+      },
+      {
+        path: ROUTES.AUTH.path,
+        title: ROUTES.AUTH.meta.title,
+        component: AuthPage,
+      },
+      {
+        path: '**',
+        title: ROUTES.NOT_FOUND.meta.title,
+        loadComponent: notFoundPage,
+      },
+    ],
+  },
+];
--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -3,10 +3,12 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { EntityService } from './services/entity.service';
+import type { TrackDataUI } from './models/common.model';
+import { Track } from './components/track/track';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterModule, JsonPipe],
+  imports: [RouterModule, JsonPipe, Track],
   selector: 'ppf-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
@@ -16,4 +18,14 @@ export class App {
   protected readonly entityService = inject(EntityService);
 
   public readonly entity = this.entityService.entityResource;
+
+  protected readonly mockTrack: TrackDataUI = {
+    id: '1',
+    album_image: 'https://placehold.co/300x300',
+    image: 'https://placehold.co/300x300',
+    artist_name: 'Mock Artist',
+    name: 'Mock Track',
+    duration: 213,
+    album_name: 'Mock Album',
+  };
 }
