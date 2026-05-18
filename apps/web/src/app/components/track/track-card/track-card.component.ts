06ad8595194829349d350e75ae0f7fab767c430e
savryna <assavr0@gmail.com>
2026-05-18 10:23:42 +0300

    feat: add track card view component

--- apps/web/src/app/components/track/track-card/track-card.component.ts
+++ apps/web/src/app/components/track/track-card/track-card.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import type { TrackData } from '../../../models/track-data-ui.model';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'ppf-track-card',
-  imports: [],
+  imports: [MatIcon, MatIconButton],
   templateUrl: './track-card.component.html',
   styleUrl: './track-card.component.scss',
 })
-export class TrackCardComponent {}
+export class TrackCardComponent {
+  public readonly isPlaying = input.required<boolean>();
+  public readonly track = input.required<TrackData>();
+}
