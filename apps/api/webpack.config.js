7093b056bfc7c174a0148342b835bc18b0628c1c
tryproxy <tryproxy@protonmail.com>
2026-05-21 16:15:21 +0400

    fix(api/webpack): revert to commons.js

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,24 +1,26 @@
-import { join } from 'path';
+const { join } = require('path');
 
-import { NxAppWebpackPlugin } from '@nx/webpack/app-plugin';
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 
-export const output = {
-  path: join(__dirname, '../../dist/apps/api'),
-  clean: true,
-  ...(process.env.NODE_ENV !== 'production' && {
-    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-  }),
+module.exports = {
+  output: {
+    path: join(__dirname, '../../dist/apps/api'),
+    clean: true,
+    ...(process.env.NODE_ENV !== 'production' && {
+      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    }),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: true,
+      sourceMap: true,
+    }),
+  ],
 };
-export const plugins = [
-  new NxAppWebpackPlugin({
-    target: 'node',
-    compiler: 'tsc',
-    main: './src/main.ts',
-    tsConfig: './tsconfig.app.json',
-    assets: ['./src/assets'],
-    optimization: false,
-    outputHashing: 'none',
-    generatePackageJson: true,
-    sourceMap: true,
-  }),
-];
