2876ad49a5ab3cc3677eb8db49397063beed760d
savryna <assavr0@gmail.com>
2026-05-23 16:30:23 +0300

    chore: move routes file to core/congif

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import type { Route } from '@angular/router';
-import { ROUTES } from './shared/config/routes';
+import { ROUTES } from './core/config/routes';
 import { AppLayoutComponent as AppLayout } from './core/layouts/app-layout/app-layout.component';
 import { HomePageComponent as HomePage } from './pages/home/home-page.component';
 import { AuthPageComponent as AuthPage } from './pages/auth/auth-page.component';
--- apps/web/src/app/core/guards/auth-guard.ts
+++ apps/web/src/app/core/guards/auth-guard.ts
@@ -1,7 +1,7 @@
 import { inject } from '@angular/core';
 import type { CanMatchFn } from '@angular/router';
 import { Router } from '@angular/router';
-import { ROUTES } from '../../shared/config/routes';
+import { ROUTES } from '../config/routes';
 import { IS_AUTHED } from '../config/mock-flags';
 
 export const authGuard: CanMatchFn = () => {
