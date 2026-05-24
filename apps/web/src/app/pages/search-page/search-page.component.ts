a39b0434e30a4af748ae0c8bd9dd8c730cc1102b
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-05-24 22:12:33 +0500

    feat: add more correct rows on search-page

--- apps/web/src/app/pages/search-page/search-page.component.ts
+++ apps/web/src/app/pages/search-page/search-page.component.ts
@@ -1,19 +1,15 @@
 import { ChangeDetectionStrategy, Component, effect, inject, viewChild } from '@angular/core';
 import { TrackService } from '../../features/tracks/services/track.mock.service';
-
 import { MatFormField, MatInput, MatInputModule, MatLabel } from '@angular/material/input';
-
-// TABLE FILTER
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-
-// TABLE SORT
 import type { Sort } from '@angular/material/sort';
-import { MatSortModule } from '@angular/material/sort';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { MatSort } from '@angular/material/sort';
-
-// TABLE PAGINATOR
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { DurationPipe } from '../../shared/pipes/duration.pipe';
+import { AbbreviatedNumberPipe } from '../../shared/pipes/abbreviated-number.pipe';
 
 @Component({
   selector: 'ppf-search-page',
@@ -25,7 +21,10 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
     MatLabel,
     MatInput,
     MatInputModule,
-    MatFormField,
+    MatIconModule,
+    MatButtonModule,
+    DurationPipe,
+    AbbreviatedNumberPipe,
   ],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
@@ -38,22 +37,33 @@ export class PpfSearchPageComponent {
 
   public dataSource = new MatTableDataSource(this.trackList());
 
-  public applyFilter(event: Event): void {
-    const filterValue = (event.target as HTMLInputElement).value;
-
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  public displayedColumns: string[] = ['album name', 'artist meta', 'date'];
+  public displayedColumns: string[] = [
+    'album_cover',
+    'artist_meta',
+    'play_count',
+    'duration',
+    'play',
+  ];
 
   public readonly sort = viewChild(MatSort);
   public readonly paginator = viewChild(MatPaginator);
 
   constructor() {
     effect(() => {
-      this.dataSource.sort = this.sort();
-      this.dataSource.paginator = this.paginator();
+      this.dataSource.data = this.trackList();
+      this.dataSource.sort = this.sort() ?? null;
+      this.dataSource.paginator = this.paginator() ?? null;
     });
+  }
+
+  public applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   public announceSortChange(sortState: Sort): void {
