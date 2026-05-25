42345145f05b3a09875bf504e2db6ac235d04578
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-05-25 15:45:28 +0500

    feat: add signal for tracking of loading progress
    * also add preloading of metadata

--- apps/web/src/app/features/player/services/html-audio.service.ts
+++ apps/web/src/app/features/player/services/html-audio.service.ts
@@ -5,19 +5,29 @@ export class AudioEngine {
   private readonly audio = new Audio();
 
   public readonly isPlaying = signal(false);
+  public readonly buffered = signal(0);
 
   constructor() {
     const currentAudio = this.audio;
 
+    currentAudio.preload = 'metadata';
+
     const onPlay = (): void => this.isPlaying.set(true);
     const onPause = (): void => this.isPlaying.set(false);
+    const onProgress = (): void => {
+      if (this.buffered.length >= 1) {
+        this.buffered.set(currentAudio.buffered.end(currentAudio.buffered.length - 1));
+      }
+    };
 
     currentAudio.addEventListener('play', onPlay);
     currentAudio.addEventListener('pause', onPause);
+    currentAudio.addEventListener('progress', onProgress);
 
     inject(DestroyRef).onDestroy(() => {
       currentAudio.removeEventListener('play', onPlay);
       currentAudio.removeEventListener('pause', onPause);
+      currentAudio.removeEventListener('progress', onProgress);
     });
   }
 
