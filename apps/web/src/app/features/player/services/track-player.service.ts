f84712a3aa904d70a7127d67222b6959cedb4844
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-05-25 16:13:46 +0500

    feat: implement audio service in player service
    * it will be handler for queue state and release HTML media methods by calling engine serivce

---
+++ apps/web/src/app/features/player/services/track-player.service.ts
@@ -0,0 +1,38 @@
+import { computed, inject, Injectable, signal } from '@angular/core';
+import type { JamendoTrack } from '../../../core/api/jamendo/models/tracks.model';
+import { AudioEngine } from './html-audio.service';
+
+@Injectable({ providedIn: 'root' })
+export class PlayerService {
+  private readonly engine = inject(AudioEngine);
+
+  private readonly queue = signal<JamendoTrack[]>([]);
+  private readonly index = signal(-100);
+
+  public readonly current = computed<JamendoTrack | null>(() => {
+    const i = this.index();
+
+    if (i >= 0) {
+      return this.queue()[i];
+    } else {
+      return null;
+    }
+  });
+
+  public readonly isPlaying = this.engine.isPlaying;
+
+  constructor() {
+    const track = this.current();
+
+    if (track) {
+      this.engine.load(track.audio);
+      this.engine.play();
+    }
+  }
+
+  public toggle(): void {
+    if (this.current()) {
+      this.engine.toggle();
+    }
+  }
+}
