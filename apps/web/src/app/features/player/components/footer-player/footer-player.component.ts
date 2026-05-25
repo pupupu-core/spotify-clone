0d7bb7d4d329f6c61de33b093899db77ff4de6ed
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-05-25 14:45:52 +0500

    feat: add MatSlider and MatSliderThumb

--- apps/web/src/app/features/player/components/footer-player/footer-player.component.ts
+++ apps/web/src/app/features/player/components/footer-player/footer-player.component.ts
@@ -2,11 +2,13 @@ import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core
 import type { JamendoTrack } from '../../../../core/api/jamendo/models/tracks.model';
 import { DurationPipe } from '../../../../shared/pipes/duration.pipe';
 import { MatIcon } from '@angular/material/icon';
+import { MatSlider } from '@angular/material/slider';
+import { MatSliderThumb } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'ppf-footer-player',
-  imports: [DurationPipe, MatIcon, MatButtonModule],
+  imports: [DurationPipe, MatIcon, MatSlider, MatSliderThumb, MatButtonModule],
   templateUrl: './footer-player.component.html',
   styleUrl: './footer-player.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
