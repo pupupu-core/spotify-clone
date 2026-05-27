8984ce5f87ef738901466ef29b096c00d2e960b9
tryproxy <tryproxy@protonmail.com>
2026-05-28 02:57:19 +0400

    chore(api): add throttler

--- apps/api/src/app/gateway/rest/v1/auth/auth.module.ts
+++ apps/api/src/app/gateway/rest/v1/auth/auth.module.ts
@@ -8,8 +8,12 @@ import { IssueAuthSessionStep } from '../../../../core/steps/issue-auth-session.
 import { LoginUserWorkflow } from '../../../../core/workflows/auth/login-user.workflow';
 import { LogoutUserWorkflow } from '../../../../core/workflows/auth/logout-user.workflow';
 import { InvalidateUserSessionStep } from '../../../../core/steps/invalidate-user-session.step';
-import { RegisterWorkflow } from '../../../../core/workflows/auth/register-user.workflow';
+import { RegisterUserWorkflow } from '../../../../core/workflows/auth/register-user.workflow';
 import { RefreshUserSessionWorkflow } from '$/core/workflows/auth/refresh-user-session.workflow';
+import { HashUserPasswordStep } from '$/core/steps/hash-user-password.step';
+import { EnsureUserEmailIsAvailableStep } from '$/core/steps/ensure-user-email-is-available.step';
+import { CreateUserAccountStep } from '$/core/steps/create-user-account.step';
+import { AuthCookieService } from './auth-cookie.service';
 
 @Module({
   imports: [PrismaModule],
@@ -17,12 +21,16 @@ import { RefreshUserSessionWorkflow } from '$/core/workflows/auth/refresh-user-s
   providers: [
     LoginUserWorkflow,
     LogoutUserWorkflow,
-    RegisterWorkflow,
+    RegisterUserWorkflow,
     RefreshUserSessionWorkflow,
     FindUserForLoginStep,
     VerifyLoginPasswordStep,
     IssueAuthSessionStep,
     InvalidateUserSessionStep,
+    HashUserPasswordStep,
+    EnsureUserEmailIsAvailableStep,
+    CreateUserAccountStep,
+    AuthCookieService,
   ],
 })
 export class AuthModule {}
