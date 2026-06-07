bf4e9e11aad3f7dad53c2fb33a32914235f3ef79
savryna <assavr0@gmail.com>
2026-06-07 19:08:17 +0300

    refactor: add empty array for playlist shelf, delete playlist-shelf html

--- apps/web/src/app/pages/home/home-page.component.ts
+++ apps/web/src/app/pages/home/home-page.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { TrackListComponent } from '../../features/tracks/components/track-list/track-list.component';
 import { TrackService } from '../../features/tracks/services/track.mock.service';
-import { PlaylistShelfComponent } from '../../shared/ui/playlist/playlist-shelf/playlist-shelf.component';
 
 @Component({
   selector: 'ppf-home-page',
-  imports: [TrackListComponent, PlaylistShelfComponent],
+  imports: [TrackListComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
