95e02d2389e02842cc9841abbd4a78caa36e28f9
savryna <assavr0@gmail.com>
2026-06-11 23:40:29 +0300

    feat: add track list and album playlist

--- apps/web/src/app/pages/artist/artist-page.component.ts
+++ apps/web/src/app/pages/artist/artist-page.component.ts
@@ -1,12 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
+import { TrackListComponent } from '~/features/tracks/components/track-list/track-list.component';
+import { TrackService } from '~/features/tracks/services/track.mock.service';
+import { PlaylistShelfComponent } from '~/shared/ui/playlist/playlist-shelf/playlist-shelf.component';
 
 @Component({
   selector: 'ppf-artist-page',
-  imports: [NgOptimizedImage, MatIcon],
+  imports: [NgOptimizedImage, MatIcon, TrackListComponent, PlaylistShelfComponent],
   templateUrl: './artist-page.component.html',
   styleUrl: './artist-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ArtistPageComponent {}
+export class ArtistPageComponent {
+  private readonly trackService = inject(TrackService);
+  public readonly trackList = this.trackService.trackList;
+}
