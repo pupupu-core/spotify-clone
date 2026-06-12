6d6432391a2e862e26a954d758674ed0772d7b43
savryna <assavr0@gmail.com>
2026-06-12 19:08:10 +0300

    refactor: move artist interfaces to shared model package

--- apps/web/src/app/core/mocks/artists.mocks.ts
+++ apps/web/src/app/core/mocks/artists.mocks.ts
@@ -1,40 +1,25 @@
-import type {
-  JamendoArtistTrack,
-  JamendoArtistTracksResponse,
-  JamendoArtistTracksResults,
-} from '../api/jamendo/models/artists.model';
+import type { ArtistTrack, ArtistTracksResults } from '@streaming-service/model';
 
-export const ARTIST_TRACK_MOCK: JamendoArtistTrack = {
-  album_id: '104336',
-  album_name: 'Season One',
+export const ARTIST_TRACK_MOCK: ArtistTrack = {
+  albumId: '104336',
+  albumName: 'Season One',
   id: '887211',
   name: 'City',
   duration: '197',
-  releasedate: '2011-12-29',
-  license_ccurl: 'http://creativecommons.org/licenses/by-nc-sa/3.0/',
-  album_image: 'https://usercontent.jamendo.com?type=album&id=104336&width=300&trackid=887211',
-  image: 'https://usercontent.jamendo.com?type=album&id=104336&width=300&trackid=887211',
-  audio: 'https://prod-1.storage.jamendo.com/?trackid=887211&format=mp31&from=app-devsite',
-  audiodownload: 'https://prod-1.storage.jamendo.com/download/track/887211/mp31/',
-  audiodownload_allowed: true,
+  releaseDate: '2011-12-29',
+  licenseUrl: 'http://creativecommons.org/licenses/by-nc-sa/3.0/',
+  albumImageUrl: 'https://usercontent.jamendo.com?type=album&id=104336&width=300&trackid=887211',
+  imageUrl: 'https://usercontent.jamendo.com?type=album&id=104336&width=300&trackid=887211',
+  audioUrl: 'https://prod-1.storage.jamendo.com/?trackid=887211&format=mp31&from=app-devsite',
+  audioDownloadUrl: 'https://prod-1.storage.jamendo.com/download/track/887211/mp31/',
+  audioDownloadAllowed: true,
 };
 
-export const ARTISTS_TRACKS_RESULTS_MOCK: JamendoArtistTracksResults = {
+export const ARTISTS_TRACKS_RESULTS_MOCK: ArtistTracksResults = {
   id: '376782',
   name: 'WE ARE FM',
   website: 'https://www.facebook.com/wearefm',
-  joindate: '2011-12-29',
-  image: 'https:usercontent.jamendo.com?type=artist&id=376782&width=300',
+  joinDate: '2011-12-29',
+  imageUrl: 'https:usercontent.jamendo.com?type=artist&id=376782&width=300',
   tracks: [ARTIST_TRACK_MOCK],
-};
-
-export const ARTIST_RESPONSE_MOCK: JamendoArtistTracksResponse = {
-  headers: {
-    status: 'success',
-    code: 0,
-    error_message: '',
-    warnings: '',
-    results_count: 2,
-  },
-  results: [ARTISTS_TRACKS_RESULTS_MOCK],
 };
