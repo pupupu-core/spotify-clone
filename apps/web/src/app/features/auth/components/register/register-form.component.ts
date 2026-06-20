6cb2d87241c96bbc9e877d78b0918fca0a4a2c61
tryproxy <tryproxy@protonmail.com>
2026-06-20 19:50:51 +0300

    feat(web): generate feature register form component

---
+++ apps/web/src/app/features/auth/components/register/register-form.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ppf-register-form',
+  imports: [],
+  templateUrl: './register-form.component.html',
+  styleUrl: './register-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterFormComponent {}
