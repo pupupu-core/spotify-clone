5b0793684fe4101d2c7ce759382f5cc850dad27d
savryna <assavr0@gmail.com>
2026-06-22 13:10:22 +0300

    fix: fix artist input type

--- apps/api/src/app/infrastructure/jamendo/types/artist-input.ts
+++ apps/api/src/app/infrastructure/jamendo/types/artist-input.ts
@@ -1,13 +1,39 @@
-import { JamendoListTracksOrder } from '$/infrastructure/jamendo/types/track-input';
-
 export interface JamendoArtistTracksInput {
-  order: JamendoListTracksOrder;
+  order: JamendoArtistTracksOrder;
   artistId: number[];
   limit?: number;
 }
 
 export interface JamendoArtistAlbumsInput {
-  order: JamendoListTracksOrder;
+  order: JamendoArtistAlbumsOrder;
   artistId: number[];
   limit?: number;
 }
+
+type JamendoArtistSortDirection = 'asc' | 'desc';
+
+type JamendoArtistRatingOrder = 'popularity_total' | 'popularity_month' | 'popularity_week';
+
+type JamendoArtistBaseOrderField = 'name' | 'id' | 'joindate';
+
+type JamendoArtistTrackOrderField =
+  | JamendoArtistBaseOrderField
+  | 'track_name'
+  | 'track_id'
+  | 'track_releasedate';
+
+export type JamendoArtistTracksOrder =
+  | JamendoArtistRatingOrder
+  | JamendoArtistTrackOrderField
+  | `${JamendoArtistTrackOrderField}_${JamendoArtistSortDirection}`;
+
+type JamendoArtistAlbumOrderField =
+  | JamendoArtistBaseOrderField
+  | 'album_name'
+  | 'album_id'
+  | 'album_releasedate';
+
+export type JamendoArtistAlbumsOrder =
+  | JamendoArtistRatingOrder
+  | JamendoArtistAlbumOrderField
+  | `${JamendoArtistAlbumOrderField}_${JamendoArtistSortDirection}`;
