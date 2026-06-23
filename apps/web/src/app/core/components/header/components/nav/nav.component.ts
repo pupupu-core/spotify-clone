f464407074acc6c3c9805a2db385371949cc542e
savryna <assavr0@gmail.com>
2026-06-23 22:43:23 +0500

    fix: fix bg for nav menu with router url

--- apps/web/src/app/core/components/header/components/nav/nav.component.ts
+++ apps/web/src/app/core/components/header/components/nav/nav.component.ts
@@ -11,10 +11,10 @@ import { APP_ROUTES } from '~/core/tokens/app-routes.token';
 import { AuthSessionService } from '~/core/stores/auth-session.service';
 
 const NAV_ITEMS: NavItem[] = [
-  { label: ROUTES.HOME.meta.title, path: ROUTES.HOME.to, backgroundImage: 'home' },
-  { label: ROUTES.SEARCH.meta.title, path: ROUTES.SEARCH.to, backgroundImage: 'search' },
-  { label: ROUTES.LIBRARY.meta.title, path: ROUTES.LIBRARY.to, backgroundImage: 'library' },
-  { label: ROUTES.ABOUT_US.meta.title, path: ROUTES.ABOUT_US.to, backgroundImage: 'about-us' },
+  { label: ROUTES.HOME.meta.title, path: ROUTES.HOME.to, backgroundImage: '/' },
+  { label: ROUTES.SEARCH.meta.title, path: ROUTES.SEARCH.to, backgroundImage: '/search' },
+  { label: ROUTES.LIBRARY.meta.title, path: ROUTES.LIBRARY.to, backgroundImage: '/library' },
+  { label: ROUTES.ABOUT_US.meta.title, path: ROUTES.ABOUT_US.to, backgroundImage: '/about-us' },
 ];
 
 interface NavItem {
@@ -45,7 +45,7 @@ export class NavComponent {
   private readonly routes = inject(APP_ROUTES);
 
   public readonly logoutClick = output<void>();
-  protected readonly hoveredRouter = signal<NavItem>(NAV_ITEMS[0]);
+  protected readonly hoveredRouter = signal<string>(this.router.url);
   protected readonly navItems = NAV_ITEMS;
   protected readonly APP_NAME = APP_NAME;
 
