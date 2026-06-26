06900589864413d4ded75dc6202a5b1b8ef38222
tryproxy <tryproxy@protonmail.com>
2026-06-27 02:12:31 +0300

    refactor(api:infrastrucutre/storage): rename config

--- apps/api/src/app/infrastructure/storage/s3-storage.config.ts
+++ apps/api/src/app/infrastructure/storage/s3-storage.config.ts
@@ -1,7 +1,7 @@
 import { APP_CONFIG } from '$/shared/config/app.config';
 import { type S3ClientConfig } from '@aws-sdk/client-s3';
 
-export const S3_CLIENT_CONFIG = {
+export const S3_STORAGE_CONFIG = {
   endpoint: APP_CONFIG.storage.s3.endpoint,
   region: APP_CONFIG.storage.s3.region,
   forcePathStyle: APP_CONFIG.storage.s3.forcePathStyle,
