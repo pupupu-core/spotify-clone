1d493a94688abb0a9c0230be49014031dbad9c96
savryna <assavr0@gmail.com>
2026-06-26 21:57:37 +0500

    feat: add trackUI interface

---
+++ apps/web/src/app/shared/models/track-ui.model.ts
@@ -0,0 +1,11 @@
+export interface TrackUI {
+  id: string;
+  name: string;
+  duration: number;
+  artistId: string;
+  artistName: string;
+  releaseDate: string;
+  imageUrl: string;
+  albumImageUrl: string;
+  audioUrl: string;
+}
