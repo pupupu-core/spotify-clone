8696de54accf2b59e2a875e3a591b745b3a031e9
tryproxy <tryproxy@protonmail.com>
2026-06-27 02:15:47 +0300

    feat(config): add upload track constraints

--- packages/shared/config/src/lib/api/constraints.ts
+++ packages/shared/config/src/lib/api/constraints.ts
@@ -17,11 +17,18 @@ export const AUTH_CONSTRAINTS = {
 export const UPLOAD_TRACK_CONSTRAINTS = {
   title: {
     minLength: 1,
+    maxLength: 120,
   },
   artistName: {
     minLength: 1,
+    maxLength: 120,
   },
   albumName: {
     minLength: 1,
+    maxLength: 120,
+  },
+  limits: {
+    maxFileSizeBytes: 10 * 1024 * 1024, // 10MB
+    typeRegex: /^audio\/(mpeg|mp3|wav|ogg|flac|aac|x-m4a|mp4)$/,
   },
 };
