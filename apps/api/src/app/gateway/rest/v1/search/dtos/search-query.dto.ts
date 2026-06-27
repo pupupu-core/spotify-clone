aa4197bbc5e2017360194622e44763c4ad9e93e5
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-06-27 11:31:48 +0500

    feat: provide search endpoints

---
+++ apps/api/src/app/gateway/rest/v1/search/dtos/search-query.dto.ts
@@ -0,0 +1,53 @@
+import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsInt, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
+
+export class AutocompleteQueryDto {
+  @ApiProperty({
+    description: 'autocompete suggestion prefixes (he... -> hello/hell/hellium)',
+    minLength: 2,
+    maxLength: 100,
+    example: 'rock',
+  })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
+  public query: string;
+
+  @ApiPropertyOptional({
+    description: 'how much suggestions are matched per query',
+    minimum: 1,
+    maximum: 10,
+    default: 5,
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(10)
+  public limit = 5;
+}
+
+export class TrackSearchQueryDto {
+  @ApiProperty({
+    description: 'search by text/track/album/tag',
+    minLength: 1,
+    maxLength: 100,
+    example: 'rock',
+  })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(100)
+  public query: string;
+
+  @ApiPropertyOptional({
+    description: 'maximum returned tracks',
+    minimum: 1,
+    maximum: 100,
+    default: 50,
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  public limit = 50;
+}
