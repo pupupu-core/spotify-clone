4ab42309b41130f6514d8fb297497121d34ac834
tryproxy <tryproxy@protonmail.com>
2026-06-27 03:21:45 +0300

    fix(api:gateway): register upload track step

--- apps/api/src/app/gateway/rest/v1/track/track.module.ts
+++ apps/api/src/app/gateway/rest/v1/track/track.module.ts
@@ -7,6 +7,7 @@ import { JamendoModule } from '$/infrastructure/jamendo/jamendo.module';
 import { AccessTokenGuard } from '../../guards/access-token.guard';
 import { AuthTokenModule } from '$/infrastructure/token/auth-token.module';
 import { UploadTrackWorkflow } from '$/core/workflows/track/upload-track.workflow';
+import { UploadTrackStep } from '$/core/steps/upload-track.step';
 
 @Module({
   imports: [JamendoModule, AuthTokenModule],
@@ -16,6 +17,7 @@ import { UploadTrackWorkflow } from '$/core/workflows/track/upload-track.workflo
     UploadTrackWorkflow,
     FetchTrackDiscoveryStep,
     AccessTokenGuard,
+    UploadTrackStep,
   ],
 })
 export class TrackModule {}
