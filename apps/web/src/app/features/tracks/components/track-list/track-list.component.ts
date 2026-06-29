d0777b55ac5a61c0316666d419d1d5d11a149762
savryna <assavr0@gmail.com>
2026-06-29 23:30:23 +0500

    fix: fix eslint error

--- apps/web/src/app/features/tracks/components/track-list/track-list.component.ts
+++ apps/web/src/app/features/tracks/components/track-list/track-list.component.ts
@@ -1,15 +1,12 @@
 import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
 import type { TrackListMode } from './models/mode.model';
 import { Track } from '../track/track';
-import { MatIcon } from '@angular/material/icon';
-import { MatButton } from '@angular/material/button';
 import { PpfPlayerService } from '../../../player/services/track-player.service';
 import type { TrackUI } from '~/shared/models/track-ui.model';
-import type { TrackMode } from '~/features/tracks/components/track/models/track.model';
 
 @Component({
   selector: 'ppf-tracks-list',
-  imports: [Track, MatIcon, MatButton],
+  imports: [Track],
   templateUrl: './track-list.component.html',
   styleUrl: './track-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,9 +16,7 @@ export class TrackListComponent {
   public readonly trackList = input.required<TrackUI[]>();
   public readonly mode = input<TrackListMode>('list');
   protected readonly player = inject(PpfPlayerService);
-  protected readonly trackView = computed<TrackMode>(() =>
-    this.mode() === 'grid' ? 'card' : 'row',
-  );
+  protected readonly trackView = computed(() => (this.mode() === 'grid' ? 'card' : 'row'));
 
   protected playAllClick(): void {
     this.player.playTracks(this.trackList());
