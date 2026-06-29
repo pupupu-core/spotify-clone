c95de5246bc8c3fb5788c75887cc833efdfd5acd
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-06-29 21:17:34 +0500

    feat: provide some logic in ppf-search-bar
    so, when we submit form, we navigates to search page and our query is passed as a filter on search-page
    * but when we trying to search on search-page, filtering q is not working
    * i'll fix it later

--- apps/web/src/app/core/components/header/components/search-bar/search-bar.component.ts
+++ apps/web/src/app/core/components/header/components/search-bar/search-bar.component.ts
@@ -1,13 +1,60 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatFormField, MatInput, MatSuffix } from '@angular/material/input';
 import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
+import { ActivatedRoute, Router } from '@angular/router';
+import { distinctUntilChanged, map } from 'rxjs';
+import { ROUTES } from '~/core/config/routes.config';
+
+interface ppfSearchBar {
+  searchQuery: FormControl<string>;
+}
 
 @Component({
   selector: 'ppf-search-bar',
-  imports: [MatFormField, MatIcon, MatInput, MatSuffix, MatIconButton],
+  imports: [MatFormField, MatIcon, MatInput, MatSuffix, MatIconButton, ReactiveFormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchBarComponent {}
+export class SearchBarComponent {
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  private readonly destroyRef = inject(DestroyRef);
+
+  protected readonly ppfSearchBarGroup = new FormGroup<ppfSearchBar>({
+    searchQuery: new FormControl('', { nonNullable: true }),
+  });
+
+  public constructor() {
+    this.route.queryParamMap
+      .pipe(
+        map(params => params.get('q') ?? ''),
+        distinctUntilChanged(),
+        takeUntilDestroyed(this.destroyRef),
+      )
+      .subscribe(query => {
+        if (query !== this.ppfSearchBarGroup.controls.searchQuery.value) {
+          this.ppfSearchBarGroup.setValue({ searchQuery: query }, { emitEvent: false });
+        }
+      });
+  }
+
+  protected submitSearch(): void {
+    this.navigateToSearch(this.ppfSearchBarGroup.controls.searchQuery.value);
+  }
+
+  private navigateToSearch(rawQuery: string): void {
+    const query = rawQuery.trim();
+
+    if (query.length === 0) {
+      return;
+    }
+
+    void this.router.navigate([ROUTES.SEARCH.to], {
+      queryParams: { q: query },
+    });
+  }
+}
