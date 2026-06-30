ae899612ace70199a1ef87cc0f89910fab8f0df9
savryna <assavr0@gmail.com>
2026-06-30 18:37:50 +0500

    refactor: sort tracks by listenedTotal

--- apps/api/src/app/core/workflows/artist/get-track-artist.workflow.ts
+++ apps/api/src/app/core/workflows/artist/get-track-artist.workflow.ts
@@ -12,20 +12,23 @@ export class GetTrackArtistWorkflow {
 
   public async execute(artistId: number): Promise<ArtistTracksResponse> {
     const response = await this.fetchTrackArtistStep.execute(artistId);
-    const limitTracks = response.tracks.slice(0, 10);
 
-    const limitResponse: ArtistTracksResponse = { ...response, tracks: limitTracks };
-    const trackIds = limitResponse.tracks.map(track => Number(track.id));
+    const trackIds = response.tracks.map(track => Number(track.id));
     const tracks = await this.fetTrackByIds.execute(trackIds);
 
     const tracksMap = new Map(tracks.map(track => [track.id, track]));
 
-    return {
-      ...limitResponse,
-      tracks: limitResponse.tracks.map(track => ({
+    const sortedTracks = response.tracks
+      .map(track => ({
         ...track,
-        listenedTotal: tracksMap.get(track.id).stats.listenedTotal ?? 0,
-      })),
+        listenedTotal: tracksMap.get(track.id)?.stats.listenedTotal ?? 0,
+      }))
+      .sort((a, b) => b.listenedTotal - a.listenedTotal)
+      .slice(0, 10);
+
+    return {
+      ...response,
+      tracks: sortedTracks,
     };
   }
 }
