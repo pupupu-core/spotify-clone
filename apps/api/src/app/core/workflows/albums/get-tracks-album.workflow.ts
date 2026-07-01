642ce015dd8a3e75f087d9d69af17f240dc8a27a
savryna <assavr0@gmail.com>
2026-07-01 21:33:26 +0500

    feat: add stat listenedTotal for artist album tracks

--- apps/api/src/app/core/workflows/albums/get-tracks-album.workflow.ts
+++ apps/api/src/app/core/workflows/albums/get-tracks-album.workflow.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { FetchTracksAlbumStep } from '$/core/steps/fetch-tracks-album.step';
 import { AlbumResponse } from '@streaming-service/model';
+import { FetchTracksByIdStep } from '$/core/steps/fetch-tracks-by-id.step';
 
 @Injectable()
 export class GetTracksAlbumWorkflow {
-  public constructor(private readonly fetchTrackAlbumStep: FetchTracksAlbumStep) {}
+  public constructor(
+    private readonly fetchTrackAlbumStep: FetchTracksAlbumStep,
+    private readonly fetTrackByIds: FetchTracksByIdStep,
+  ) {}
 
   public async execute(albumId: number): Promise<AlbumResponse> {
-    return this.fetchTrackAlbumStep.execute(albumId);
+    const response = await this.fetchTrackAlbumStep.execute(albumId);
+
+    const trackIds = response.tracks.map(track => Number(track.trackId));
+    const tracks = await this.fetTrackByIds.execute(trackIds);
+
+    const tracksMap = new Map(tracks.map(track => [track.id, track]));
+
+    return {
+      ...response,
+      tracks: response.tracks.map(track => ({
+        ...track,
+        listenedTotal: tracksMap.get(track.trackId).stats.listenedTotal,
+      })),
+    };
   }
 }
--- packages/shared/model/src/lib/albums/album-tracks-response.model.ts
+++ packages/shared/model/src/lib/albums/album-tracks-response.model.ts
@@ -21,4 +21,5 @@ export interface AlbumTrack {
   audioUrl: string;
   audioDownloadUrl: string;
   audioDownloadAllowed: boolean;
+  listenedTotal?: number;
 }
