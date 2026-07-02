ea17d17c0c9683c7b5562aec4826fe6702f4517c
savryna <assavr0@gmail.com>
2026-07-02 12:23:48 +0500

    feat: add fullTime param for durationPipe

--- apps/web/src/app/core/constants/number.constants.ts
+++ apps/web/src/app/core/constants/number.constants.ts
@@ -1,2 +1,3 @@
 export const SECONDS_PER_MINUTE = 60;
+export const SECONDS_PER_HOURS = 3600;
 export const THOUSAND = 1_000;
--- apps/web/src/app/shared/pipes/duration.pipe.ts
+++ apps/web/src/app/shared/pipes/duration.pipe.ts
@@ -1,13 +1,14 @@
 import type { PipeTransform } from '@angular/core';
 import { Pipe } from '@angular/core';
-import { SECONDS_PER_MINUTE } from '../../core/constants/number.constants';
+import { SECONDS_PER_HOURS, SECONDS_PER_MINUTE } from '../../core/constants/number.constants';
 
 @Pipe({
   name: 'duration',
 })
 export class DurationPipe implements PipeTransform {
-  transform(time: string | number): string {
+  transform(time: string | number, format: 'fullTime' | undefined = undefined): string {
     const timeNumber = Number(time);
+    const hours = Math.trunc(timeNumber / SECONDS_PER_HOURS).toString();
     const minutes = Math.trunc(timeNumber / SECONDS_PER_MINUTE)
       .toString()
       .padStart(2, '0');
@@ -15,6 +16,14 @@ export class DurationPipe implements PipeTransform {
       .toString()
       .padStart(2, '0');
 
-    return `${minutes}:${seconds}`;
+    return format
+      ? [
+          Number(hours) > 0 ? `${hours}h` : null,
+          Number(minutes) > 0 ? `${minutes}min` : null,
+          Number(seconds) > 0 ? `${seconds}s` : null,
+        ]
+          .filter(Boolean)
+          .join(' ')
+      : `${minutes}:${seconds}`;
   }
 }
