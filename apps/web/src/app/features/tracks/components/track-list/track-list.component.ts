f310674ead2d3317853c547d72171863892a6a8e
savryna <assavr0@gmail.com>
2026-07-03 00:23:00 +0500

    feat: add sort last played button

--- apps/web/src/app/features/tracks/components/track-list/track-list.component.ts
+++ apps/web/src/app/features/tracks/components/track-list/track-list.component.ts
@@ -1,15 +1,24 @@
-import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
-import type { TrackListMode, UlMode } from './models/mode.model';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  output,
+  signal,
+} from '@angular/core';
+import type { PageMode, TrackListMode, UlMode } from './models/mode.model';
 import { Track } from '../track/track';
 import { PpfPlayerService } from '../../../player/services/track-player.service';
 import type { TrackUI } from '~/shared/models/track-ui.model';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { NgTemplateOutlet } from '@angular/common';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'ppf-tracks-list',
-  imports: [Track, MatButton, MatIcon, NgTemplateOutlet],
+  imports: [Track, MatButton, MatIcon, NgTemplateOutlet, MatSortModule, MatFabButton],
   templateUrl: './track-list.component.html',
   styleUrl: './track-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,8 +28,11 @@ export class TrackListComponent {
   public readonly trackList = input.required<TrackUI[]>();
   public readonly mode = input<TrackListMode>('list');
   public readonly ulMode = input.required<UlMode>();
+  public readonly pageMode = input<PageMode>();
   protected readonly player = inject(PpfPlayerService);
   protected readonly trackView = computed(() => (this.mode() === 'grid' ? 'card' : 'row'));
+  protected readonly sortChange = output<'asc' | 'desc'>();
+  protected readonly direction = signal<'asc' | 'desc'>('desc');
 
   protected playAllClick(): void {
     this.player.playTracks(this.trackList());
@@ -28,5 +40,12 @@ export class TrackListComponent {
 
   protected playTrackClick(track: TrackUI): void {
     this.player.toggleTrackByID(track, this.trackList());
+  }
+
+  public toggleSort(): void {
+    const next = this.direction() === 'asc' ? 'desc' : 'asc';
+
+    this.direction.set(next);
+    this.sortChange.emit(next);
   }
 }
