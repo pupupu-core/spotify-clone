7580c42f919127834bccd6a1cefb1bccd6352f94
tryproxy <tryproxy@protonmail.com>
2026-07-04 19:58:12 +0300

    feat(web:pages, web:features): add loader to discover page with error state

--- apps/web/src/app/pages/home/home-page.component.ts
+++ apps/web/src/app/pages/home/home-page.component.ts
@@ -1,20 +1,19 @@
 import type { OnInit } from '@angular/core';
 import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from '@angular/core';
-import { TrackListComponent } from '../../features/tracks/components/track-list/track-list.component';
-import { TrackService } from '../../features/tracks/services/track.mock.service';
-import type { AccountMeResponse } from '@streaming-service/model';
-import { catchError, EMPTY, type Observable, tap } from 'rxjs';
-import { AccountApiService } from '~/core/services/account-api.service';
+import { catchError, EMPTY, tap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { GenresComponent } from '~/features/genres/components/genres.component';
 import { GenresService } from '~/features/genres/services/genres.service';
 import type { TrackUI } from '~/shared/models/track-ui.model';
 import { mapTrackResponseToTrackUI } from '~/shared/utils/mappers/track.mappers';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { TrackListComponent } from '~/features/tracks/components/track-list/track-list.component';
+import { TrackService } from '~/features/tracks/services/track.service';
+import { LoaderComponent } from '~/shared/ui/loader/loader.component';
 
 @Component({
   selector: 'ppf-home-page',
-  imports: [TrackListComponent, GenresComponent, MatGridListModule],
+  imports: [TrackListComponent, GenresComponent, MatGridListModule, LoaderComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,23 +21,15 @@ import { MatGridListModule } from '@angular/material/grid-list';
 export class HomePageComponent implements OnInit {
   private readonly trackService = inject(TrackService);
   private readonly genresService = inject(GenresService);
-  // FOR DEBUG to showcase me endpoint
   private readonly destroyRef = inject(DestroyRef);
 
-  public readonly popularTracks = signal<TrackUI[] | null>(null);
-  public readonly newReleaseTracks = signal<TrackUI[] | null>(null);
+  public readonly discoverStatus = signal<'idle' | 'loading' | 'success' | 'error'>('idle');
+  public readonly popularTracks = signal<TrackUI[]>([]);
+  public readonly newReleaseTracks = signal<TrackUI[]>([]);
   public readonly genres = this.genresService.genreList;
 
-  // FOR DEBUG to showcase me endpoint
-  private readonly accountSession = inject(AccountApiService);
-
-  // FOR DEBUG to showcase me endpoint
-  protected fetchMeAccount(): Observable<AccountMeResponse> {
-    return this.accountSession.me();
-  }
-
-  // FOR DEBUG to showcase me endpoint
   public ngOnInit(): void {
+    this.discoverStatus.set('loading');
     this.trackService
       .fetchDiscover()
       .pipe(
@@ -46,21 +37,11 @@ export class HomePageComponent implements OnInit {
         tap(discovery => {
           this.popularTracks.set(discovery.popularTracks.map(mapTrackResponseToTrackUI));
           this.newReleaseTracks.set(discovery.newReleases.map(mapTrackResponseToTrackUI));
+          this.discoverStatus.set('success');
         }),
         catchError(error => {
           console.error(error);
-
-          return EMPTY;
-        }),
-      )
-      .subscribe();
-
-    this.fetchMeAccount()
-      .pipe(
-        takeUntilDestroyed(this.destroyRef),
-        tap(me => console.log(me)),
-        catchError(error => {
-          console.error(error);
+          this.discoverStatus.set('error');
 
           return EMPTY;
         }),
