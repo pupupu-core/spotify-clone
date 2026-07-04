6a5f4248e892bb180a784245d3e01bde145fc847
tryproxy <tryproxy@protonmail.com>
2026-07-04 17:59:50 +0300

    feat(web:features): use mock data for genres

--- apps/web/src/app/features/genres/services/genres.service.ts
+++ apps/web/src/app/features/genres/services/genres.service.ts
@@ -1,17 +1,16 @@
 import { Injectable, signal } from '@angular/core';
+import { pickRandomItemFromList } from '@streaming-service/utils';
+import { MUSIC_GENRES_MOCK } from '~/core/mocks/tracks.mocks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GenresService {
-  private readonly genresState = signal<string[]>([
-    // genres are hardcoded for mvp
-    // TODO fetch jamendo genres and get most popular
-    'rock',
-    'latin',
-    'pop',
-    'Electronic',
-    'Hip-Hop',
-  ]);
+  // genres are hardcoded for mvp
+  // TODO fetch jamendo genres and get most popular
+  private readonly genresState = signal<(typeof MUSIC_GENRES_MOCK)[number][]>(
+    pickRandomItemFromList(MUSIC_GENRES_MOCK, 10),
+  );
+
   public readonly genreList = this.genresState.asReadonly();
 }
