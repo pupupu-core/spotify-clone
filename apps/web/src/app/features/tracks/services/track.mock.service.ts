3f4820d8f5d22fd63c12b1af10d11cce79570eb1
tryproxy <tryproxy@protonmail.com>
2026-07-04 05:07:35 +0300

    feat(web:core, web:features): add fetch discover endpoints to track service

--- apps/web/src/app/features/tracks/services/track.mock.service.ts
+++ apps/web/src/app/features/tracks/services/track.mock.service.ts
@@ -1,11 +1,19 @@
-import { Injectable, signal } from '@angular/core';
-import { TRACKS_MOCK } from '~/core/mocks/tracks.mocks';
-import type { TrackUI } from '~/shared/models/track-ui.model';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { type TrackDiscoveryResponse } from '@streaming-service/model';
+import type { Observable } from 'rxjs';
+import { APP_ENDPOINTS } from '~/core/config/endpoints.config';
+// import { TRACKS_MOCK } from '~/core/mocks/tracks.mocks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TrackService {
-  private readonly trackListState = signal<TrackUI[]>(TRACKS_MOCK);
-  public readonly trackList = this.trackListState.asReadonly();
+  private readonly http = inject(HttpClient);
+  // private readonly trackListState = signal<TrackResponse[]>(TRACKS_MOCK);
+  // public readonly trackList = this.trackListState.asReadonly();
+
+  public fetchDiscover(): Observable<TrackDiscoveryResponse> {
+    return this.http.get<TrackDiscoveryResponse>(APP_ENDPOINTS.DISCOVER.TRACKS);
+  }
 }
