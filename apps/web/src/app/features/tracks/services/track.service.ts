dcdec85b9376a2a0ff475888b9b1bb2eec1f3f65
tryproxy <tryproxy@protonmail.com>
2026-07-04 19:55:55 +0300

    refactor(web:features): remove mock from track service name

--- apps/web/src/app/features/tracks/services/track.service.ts
+++ apps/web/src/app/features/tracks/services/track.service.ts
@@ -3,15 +3,12 @@ import { inject, Injectable } from '@angular/core';
 import { type TrackDiscoveryResponse } from '@streaming-service/model';
 import type { Observable } from 'rxjs';
 import { APP_ENDPOINTS } from '~/core/config/endpoints.config';
-// import { TRACKS_MOCK } from '~/core/mocks/tracks.mocks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TrackService {
   private readonly http = inject(HttpClient);
-  // private readonly trackListState = signal<TrackResponse[]>(TRACKS_MOCK);
-  // public readonly trackList = this.trackListState.asReadonly();
 
   public fetchDiscover(): Observable<TrackDiscoveryResponse> {
     return this.http.get<TrackDiscoveryResponse>(APP_ENDPOINTS.DISCOVER.TRACKS);
