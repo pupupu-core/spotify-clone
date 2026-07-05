07b8faa7decd96c5812bfbc03a035bb1fb41281b
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-07-06 00:59:37 +0500

    feat: add fancy 404 page
    * now i should provide same instance of the player

--- apps/web/src/app/pages/not-found/not-found-page.component.ts
+++ apps/web/src/app/pages/not-found/not-found-page.component.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ROUTES } from '~/core/config/routes.config';
 
 @Component({
   selector: 'ppf-not-found-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotFoundPageComponent {}
+export class NotFoundPageComponent {
+  protected readonly discoveryPath = ROUTES.HOME.to;
+}
