28ac8f5aea77b6bc3b042fbb858756edb51d3171
tryproxy <tryproxy@protonmail.com>
2026-07-06 02:45:15 +0300

    feat(web:pages/login, web:features/login): add style to form with Material components, add shake animation on error

--- apps/web/src/app/features/auth/components/login/login-form.component.ts
+++ apps/web/src/app/features/auth/components/login/login-form.component.ts
@@ -1,11 +1,21 @@
 import { ChangeDetectionStrategy, Component, inject, input, output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatError, MatFormField, MatInput, MatLabel } from '@angular/material/input';
 import type { LoginRequest } from '@streaming-service/model';
 import { SubmitButtonTextPipe } from '~/shared/pipes/submit-button-text.pipe';
 
 @Component({
   selector: 'ppf-login-form',
-  imports: [ReactiveFormsModule, SubmitButtonTextPipe],
+  imports: [
+    ReactiveFormsModule,
+    SubmitButtonTextPipe,
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatError,
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
