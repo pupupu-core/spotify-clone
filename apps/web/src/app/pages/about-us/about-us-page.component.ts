e6ee823e704205fb96fee1420d4c3ce7663c4ce8
savryna <assavr0@gmail.com>
2026-07-08 02:17:50 +0500

    feat: add description block

--- apps/web/src/app/pages/about-us/about-us-page.component.ts
+++ apps/web/src/app/pages/about-us/about-us-page.component.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { APP_NAME } from '~/core/constants/common.constants';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'ppf-about-us-page',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './about-us-page.component.html',
   styleUrl: './about-us-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutUsPageComponent {}
+export class AboutUsPageComponent {
+  protected readonly APP_NAME = APP_NAME;
+}
