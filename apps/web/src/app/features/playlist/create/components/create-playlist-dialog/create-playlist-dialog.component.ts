2f05e1cbb373cc6915b22156323a25bb3fee7ded
Vsevolod Timoshenko <timosh0307@gmail.com>
2026-07-08 22:51:04 +0500

    feat: provide search into playlist-dialog

--- apps/web/src/app/features/playlist/create/components/create-playlist-dialog/create-playlist-dialog.component.ts
+++ apps/web/src/app/features/playlist/create/components/create-playlist-dialog/create-playlist-dialog.component.ts
@@ -1,15 +1,21 @@
-import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { MatDialogActions, MatDialogClose, MatDialogContent } from '@angular/material/dialog';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { MatFormField, MatInput, MatLabel, MatSuffix } from '@angular/material/input';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TrackListComponent } from '~/features/tracks/components/track-list/track-list.component';
 import { fileSizeValidator } from '~/shared/validators/file-size.validator';
 import { fileTypeValidator } from '~/shared/validators/file-type.validator';
-import { startWith } from 'rxjs';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, concat, map, of, startWith, switchMap } from 'rxjs';
+import type { Observable } from 'rxjs';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { SearchBarComponent } from '~/features/search-bar/search-bar.component';
+import { SearchApiService } from '~/core/services/search-api.service';
+import type { TrackResponse } from '@streaming-service/model';
+import type { TrackUI } from '~/shared/models/track-ui.model';
+import { mapTrackResponseToTrackUI } from '~/shared/utils/mappers/track.mappers';
+import { LoaderComponent } from '~/shared/ui/loader/loader.component';
 
 const MAX_SIZE_COVER_MB = 3;
 const VALID_FILE_TYPE = ['image/jpg', 'image/png', 'image/avif', 'image/webp', 'image/jpeg'];
@@ -37,6 +43,24 @@ interface FileTypeValidationError {
   actualType: string | null;
 }
 
+// я потом вынесу в модели
+
+interface TrackSearchRequest {
+  query: string;
+}
+
+interface TrackSearchState {
+  status: 'error' | 'idle' | 'loading' | 'success';
+  query: string;
+  tracks: TrackUI[];
+}
+
+const EMPTY_TRACK_SEARCH_STATE: TrackSearchState = {
+  status: 'idle',
+  query: '',
+  tracks: [],
+};
+
 @Component({
   selector: 'ppf-create-playlist-dialog',
   imports: [
@@ -46,13 +70,13 @@ interface FileTypeValidationError {
     MatFormField,
     MatLabel,
     MatInput,
-    MatSuffix,
     ReactiveFormsModule,
     MatDialogActions,
     TrackListComponent,
+    SearchBarComponent,
     MatButton,
     MatDialogClose,
-    SearchBarComponent,
+    LoaderComponent,
   ],
   templateUrl: './create-playlist-dialog.component.html',
   styleUrl: './create-playlist-dialog.component.scss',
@@ -60,8 +84,13 @@ interface FileTypeValidationError {
 })
 export class CreatePlaylistDialogComponent {
   // TODO: добавить логику создания при появление бэка
+  private readonly searchApi = inject(SearchApiService);
+
   protected readonly VALID_FILE_TYPE = VALID_FILE_TYPE;
   protected readonly coverPreview = signal<string>('');
+  private readonly trackSearchRequest = signal<TrackSearchRequest>({
+    query: '',
+  });
 
   public readonly playlistCreateForm = new FormGroup(
     {
@@ -79,6 +108,20 @@ export class CreatePlaylistDialogComponent {
   public readonly coverFileStatus = toSignal(
     this.coverControl.statusChanges.pipe(startWith(this.coverControl.status)),
   );
+
+  protected readonly trackSearchState = toSignal(
+    toObservable(this.trackSearchRequest).pipe(
+      switchMap(({ query }) => this.loadTracksByQuery$(query)),
+    ),
+    { initialValue: EMPTY_TRACK_SEARCH_STATE },
+  );
+
+  protected readonly searchResultTitle = computed(() => {
+    const query = this.trackSearchState().query;
+
+    return query ? `Search result for "${query}"` : 'Search result';
+  });
+
   protected readonly coverError = computed<CoverErrors>(() => {
     this.coverFileStatus();
     let coverErrors: CoverErrors = {
@@ -125,6 +168,14 @@ export class CreatePlaylistDialogComponent {
     return coverErrors;
   });
 
+  protected searchTracks(query: string): void {
+    const normalizedQuery = query.trim();
+
+    this.trackSearchRequest.update(() => ({
+      query: normalizedQuery,
+    }));
+  }
+
   public setCover(event: Event): void {
     const target: HTMLInputElement = event.target as HTMLInputElement;
 
@@ -141,5 +192,37 @@ export class CreatePlaylistDialogComponent {
     }
 
     this.coverPreview.set(URL.createObjectURL(file));
+  }
+
+  private loadTracksByQuery$(rawQuery: string): Observable<TrackSearchState> {
+    const query = rawQuery.trim();
+
+    if (query.length === 0) {
+      return of(EMPTY_TRACK_SEARCH_STATE);
+    }
+
+    // нужно отрефачить, но давайте потом
+    // если честно хорошо бы от автокомплита либо отказаться, он юзлес, либо вообще подумать как отменять его после эмита запроса
+    return concat(
+      of<TrackSearchState>({
+        status: 'loading',
+        query,
+        tracks: [],
+      }),
+      this.searchApi.tracks(query).pipe(
+        map<TrackResponse[], TrackSearchState>(tracks => ({
+          status: 'success',
+          query,
+          tracks: tracks.map(track => mapTrackResponseToTrackUI(track)),
+        })),
+        catchError(() =>
+          of<TrackSearchState>({
+            status: 'error',
+            query,
+            tracks: [],
+          }),
+        ),
+      ),
+    );
   }
 }
