569988887b10f50edf44e8e6a1b3f63533fb6e7a
savryna <assavr0@gmail.com>
2026-07-09 21:39:55 +0500

    feat: add developers mini card

--- apps/web/src/app/pages/about-us/about-us-page.component.ts
+++ apps/web/src/app/pages/about-us/about-us-page.component.ts
@@ -1,6 +1,19 @@
+import type { OnInit } from '@angular/core';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { APP_NAME } from '~/core/constants/common.constants';
 import { NgOptimizedImage } from '@angular/common';
+
+type DeveloperId = 'firstDeveloper' | 'secondDeveloper' | 'thirdDeveloper' | 'fourthDeveloper';
+
+interface DeveloperInfo {
+  name: string;
+  fullName: string;
+  id: DeveloperId;
+  description: string;
+  githubLink: string;
+  imgUrl: string;
+  badge: string[];
+}
 
 @Component({
   selector: 'ppf-about-us-page',
@@ -9,6 +22,59 @@ import { NgOptimizedImage } from '@angular/common';
   styleUrl: './about-us-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutUsPageComponent {
+export class AboutUsPageComponent implements OnInit {
   protected readonly APP_NAME = APP_NAME;
+
+  public readonly developers: DeveloperInfo[] = [
+    {
+      name: 'Anastasia',
+      fullName: 'Anastasia Savrukhina',
+      id: 'firstDeveloper',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ducimus esse est ex in neque non quis ' +
+        'rerum tempora voluptatibus?',
+      githubLink: 'https://github.com/savryna',
+      imgUrl: 'https://avatars.githubusercontent.com/u/123760669?v=4',
+      badge: ['UI/UX', 'Frontend', 'Backend'],
+    },
+    {
+      name: 'Hanna',
+      fullName: 'Hanna Surmach',
+      id: 'secondDeveloper',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ducimus esse est ex in neque non quis ' +
+        'rerum tempora voluptatibus?',
+      githubLink: 'https://github.com/khasekai',
+      imgUrl: 'https://avatars.githubusercontent.com/u/58516288?v=4',
+      badge: ['Team Lead', 'Mentoring'],
+    },
+    {
+      name: 'Vsevolod',
+      fullName: 'Vsevolod Timoshenko',
+      id: 'thirdDeveloper',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ducimus esse est ex in neque non quis ' +
+        'rerum tempora voluptatibus?',
+      githubLink: 'https://github.com/shoblinsky',
+      imgUrl: 'https://avatars.githubusercontent.com/u/123806946?v=4',
+      badge: ['Frontend', 'Backend', 'Testing'],
+    },
+    {
+      name: 'Nikita',
+      fullName: 'Nikita Melnikov',
+      id: 'fourthDeveloper',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ducimus esse est ex in neque non quis ' +
+        'rerum tempora voluptatibus?',
+      githubLink: 'https://github.com/tryproxy',
+      imgUrl: 'https://avatars.githubusercontent.com/u/56947738?v=4',
+      badge: ['Frontend', 'Backend', 'Infrastructure'],
+    },
+  ];
+
+  public ngOnInit(): void {
+    document.body.style.overflow = 'hidden';
+
+    setTimeout(() => (document.body.style.overflow = ''), 1500);
+  }
 }
