9038efc624376ec2baa38b293207bf0b93d74057
savryna <assavr0@gmail.com>
2026-07-10 22:54:37 +0500

    style: fix about us

--- apps/web/src/app/pages/about-us/about-us-page.component.ts
+++ apps/web/src/app/pages/about-us/about-us-page.component.ts
@@ -15,8 +15,8 @@ export interface DeveloperInfo {
   githubLink: string;
   imgUrl: string;
   badge: string[];
-  responsibilites: string[];
-  stacks: string[];
+  responsibilities: string[];
+  stacks?: string[];
 }
 
 @Component({
@@ -39,7 +39,7 @@ export class AboutUsPageComponent implements OnInit {
       githubLink: 'https://github.com/savryna',
       imgUrl: 'https://avatars.githubusercontent.com/u/123760669?v=4',
       badge: ['UI/UX', 'Frontend', 'Backend'],
-      responsibilites: [
+      responsibilities: [
         'Figma mockup',
         'Artist, Album, Library, and About Us pages',
         'Playlist creation and editing',
@@ -58,26 +58,44 @@ export class AboutUsPageComponent implements OnInit {
       githubLink: 'https://github.com/khasekai',
       imgUrl: 'https://avatars.githubusercontent.com/u/58516288?v=4',
       badge: ['Team Lead', 'Mentoring'],
-      responsibilites: [
+      responsibilities: [
         'Team mentoring',
         'Task planning and distribution',
         'Technical guidance',
         'Learning materials preparation',
         'Trello management',
       ],
-      stacks: ['Angular', 'Trello', 'Figma'],
+    },
+    {
+      name: 'Nikita',
+      fullName: 'Nikita Melnikov',
+      id: 'thirdDeveloper',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ducimus esse est ex in neque non quis ' +
+        'rerum tempora voluptatibus?',
+      githubLink: 'https://github.com/tryproxy',
+      imgUrl: '/img/trypoxy-img.jpg',
+      badge: ['Frontend', 'Infrastructure', 'Backend'],
+      responsibilities: [
+        'Backend architecture and core API setup',
+        'Database design and Prisma schema',
+        'Swagger setup',
+        'Registration, Login and Discovery page',
+        'User track upload',
+      ],
+      stacks: ['Angular', 'RxJs', 'NestJs', 'Docker', 'Prisma', 'PostgreSQL', 'Swagger'],
     },
     {
       name: 'Vsevolod',
       fullName: 'Vsevolod Timoshenko',
-      id: 'thirdDeveloper',
+      id: 'fourthDeveloper',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ducimus esse est ex in neque non quis ' +
         'rerum tempora voluptatibus?',
       githubLink: 'https://github.com/shoblinsky',
       imgUrl: 'https://avatars.githubusercontent.com/u/123806946?v=4',
       badge: ['Frontend', 'Backend', 'Testing'],
-      responsibilites: [
+      responsibilities: [
         'Search, 404 page',
         'Music player widget',
         'Playback queue component',
@@ -86,25 +104,6 @@ export class AboutUsPageComponent implements OnInit {
         'Backend endpoint integration',
       ],
       stacks: ['Angular', 'RxJs', 'Vitest', 'NestJs'],
-    },
-    {
-      name: 'Nikita',
-      fullName: 'Nikita Melnikov',
-      id: 'fourthDeveloper',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ducimus esse est ex in neque non quis ' +
-        'rerum tempora voluptatibus?',
-      githubLink: 'https://github.com/tryproxy',
-      imgUrl: '/img/trypoxy-img.jpg',
-      badge: ['Frontend', 'Infrastructure', 'Backend'],
-      responsibilites: [
-        'Backend architecture and core API setup',
-        'Database design and Prisma schema',
-        'Swagger setup',
-        'Registration, Login and Discovery page',
-        'User track upload',
-      ],
-      stacks: ['Angular', 'RxJs', 'NestJs', 'Docker', 'Prisma', 'PostgreSQL', 'Swagger'],
     },
   ];
   public readonly selectedDeveloper = signal<DeveloperInfo | null>(null);
