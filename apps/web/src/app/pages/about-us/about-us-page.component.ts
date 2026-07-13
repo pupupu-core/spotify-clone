9007e8ffedea2e101bb7810180105a48c8fb6a46
savryna <assavr0@gmail.com>
2026-07-14 00:15:18 +0500

    feat: add developers position pipe

--- apps/web/src/app/pages/about-us/about-us-page.component.ts
+++ apps/web/src/app/pages/about-us/about-us-page.component.ts
@@ -1,15 +1,22 @@
 import type { OnInit } from '@angular/core';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { APP_NAME } from '~/core/constants/common.constants';
-import { NgOptimizedImage } from '@angular/common';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 import { CardDeveloperComponent } from '~/pages/about-us/component/card-developer/card-developer.component';
 import { CardDeveloperPreviewComponent } from '~/pages/about-us/component/card-developer-preview/card-developer-preview.component';
 import type { DeveloperInfo } from '~/pages/about-us/models/developer.model';
 import { DEVELOPERS } from '~/pages/about-us/constants/data';
+import { DeveloperPositionsPipe } from '~/pages/about-us/pipes/developer-positions.pipe';
 
 @Component({
   selector: 'ppf-about-us-page',
-  imports: [NgOptimizedImage, CardDeveloperComponent, CardDeveloperPreviewComponent],
+  imports: [
+    NgOptimizedImage,
+    CardDeveloperComponent,
+    CardDeveloperPreviewComponent,
+    NgClass,
+    DeveloperPositionsPipe,
+  ],
   templateUrl: './about-us-page.component.html',
   styleUrl: './about-us-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
