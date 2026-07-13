6df400c527b703bdaf00e8f2994cdacf087ba9a2
savryna <assavr0@gmail.com>
2026-07-14 00:02:57 +0500

    refactor: update template card developer

--- apps/web/src/app/pages/about-us/component/card-developer/card-developer.component.ts
+++ apps/web/src/app/pages/about-us/component/card-developer/card-developer.component.ts
@@ -1,15 +1,19 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
-import { LowerCasePipe, NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input, output } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { MatCard, MatCardActions, MatCardContent, MatCardTitle } from '@angular/material/card';
 import { MatDivider } from '@angular/material/list';
 import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import type { DeveloperInfo } from '~/pages/about-us/models/developer.model';
 
+interface StackBadges {
+  name: string;
+  url: string;
+}
+
 @Component({
   selector: 'ppf-card-developer',
   imports: [
-    LowerCasePipe,
     MatCard,
     MatCardActions,
     MatCardContent,
@@ -26,4 +30,14 @@ import type { DeveloperInfo } from '~/pages/about-us/models/developer.model';
 export class CardDeveloperComponent {
   public readonly developer = input.required<DeveloperInfo>();
   public readonly closeDeveloper = output();
+  public readonly githubIconUrl =
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg';
+  public readonly stackBagdes = computed((): StackBadges[] => {
+    return (this.developer().stacks ?? []).map(stack => {
+      return {
+        name: stack,
+        url: `https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${stack.toLowerCase()}/${stack.toLowerCase()}-original.svg`,
+      };
+    });
+  });
 }
