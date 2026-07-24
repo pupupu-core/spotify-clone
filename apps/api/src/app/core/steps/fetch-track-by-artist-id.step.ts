8705c17f01b017250360659d09269c521552fa46
savryna <assavr0@gmail.com>
2026-07-24 16:00:34 +0500

    feat: add retry for tracks by artistId

--- apps/api/src/app/core/steps/fetch-track-by-artist-id.step.ts
+++ apps/api/src/app/core/steps/fetch-track-by-artist-id.step.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { JamendoClient } from '$/infrastructure/jamendo/jamendo.client';
 import { TrackResponse } from '@streaming-service/model';
+import { TRACKS_CONFIG } from '$/core/models/tracks/constants';
+import { sleep } from '$/shared/lib/sleep';
 
 @Injectable()
 export class FetchTrackByArtistIdStep {
   public constructor(private readonly jamendoClient: JamendoClient) {}
 
   public async execute(artistIds: number[]): Promise<TrackResponse[]> {
-    return await this.jamendoClient.listTracks({
-      order: 'popularity_total',
-      include: ['stats'],
-      artistId: artistIds,
-    });
+    return this.fetchNonEmptyTrackList(artistIds);
+  }
+
+  private async fetchNonEmptyTrackList(artistIds: number[]): Promise<TrackResponse[]> {
+    for (let attempt = 1; attempt <= TRACKS_CONFIG.DISCOVERY.REQUEST_MAX_ATTEMPTS; attempt += 1) {
+      const tracks = await this.jamendoClient.listTracks({
+        order: 'popularity_total',
+        include: ['stats'],
+        artistId: artistIds,
+      });
+
+      if (tracks.length > 0) {
+        return tracks;
+      }
+
+      if (attempt < TRACKS_CONFIG.DISCOVERY.REQUEST_MAX_ATTEMPTS) {
+        await this.delay(TRACKS_CONFIG.DISCOVERY.RETRY_REQUEST_DELAY_MS * attempt);
+      }
+    }
+
+    throw new Error('Failed to fetch tracks after maximum retry attempts.');
+  }
+
+  private async delay(ms: number): Promise<void> {
+    await sleep(ms);
   }
 }
