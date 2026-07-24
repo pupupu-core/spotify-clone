a8f922a8bd828d2ce4373a7c77feb57f8aa28862
savryna <assavr0@gmail.com>
2026-07-24 21:51:47 +0500

    fix: handle albums with more than 10 tracks

--- apps/api/src/app/infrastructure/jamendo/jamendo.client.ts
+++ apps/api/src/app/infrastructure/jamendo/jamendo.client.ts
@@ -61,6 +61,7 @@ export class JamendoClient {
         type: input.type ?? 'single+albumtrack',
         id: input.id,
         artist_id: input.artistId,
+        album_id: input.albumId,
       },
       schema: JamendoListTracksResponseSchema,
     }).then(mapToListTracks);
