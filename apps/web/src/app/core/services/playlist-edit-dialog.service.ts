b1f8eb03e580142b5127c8168dd0f19fd372bb4e
savryna <assavr0@gmail.com>
2026-08-04 02:26:32 +0500

    feat: add my upload dialog

--- apps/web/src/app/core/services/playlist-edit-dialog.service.ts
+++ apps/web/src/app/core/services/playlist-edit-dialog.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EditPlaylistDialogComponent } from '~/features/playlist/edit/components/edit-playlist-dialog/edit-playlist-dialog.component';
+import { MyUploadDialogComponent } from '~/features/playlist/my-uploads/components/my-upload-dialog/my-upload-dialog.component';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,19 @@ export class PlaylistEditDialogService {
   private readonly dialog = inject(MatDialog);
 
   public openEditPlaylist(playlistId: string): void {
+    if (playlistId === 'my-uploads') {
+      this.dialog.open(MyUploadDialogComponent, {
+        data: {
+          name: 'My uploads',
+          description: 'Tracks you have uploaded to Pupufy.',
+        },
+        minWidth: 670,
+        minHeight: 'min-content',
+      });
+
+      return;
+    }
+
     this.dialog.open(EditPlaylistDialogComponent, {
       data: playlistId,
       minWidth: 670,
